6af082c9f1fa39adebf54ec5167985cdd02d85d6
eric.abreu <eric@dpi.inpe.br>
2016-03-13 17:47:17 -0300

    create flow diagram interface

--- src/fiocruz/flow/qt/FlowDiagramDialog.cpp
+++ src/fiocruz/flow/qt/FlowDiagramDialog.cpp
@@ -24,6 +24,11 @@ TerraLib Team at <terralib-team@terralib.org>.
 */
 
 // TerraLib
+#include <terralib/common/STLUtils.h>
+#include <terralib/dataaccess/datasource/DataSourceFactory.h>
+#include <terralib/dataaccess/datasource/DataSourceInfoManager.h>
+#include <terralib/dataaccess/datasource/DataSourceManager.h>
+#include <terralib/qt/widgets/datasource/selector/DataSourceSelectorDialog.h>
 #include <terralib/graph/builder/FlowGraphBuilder.h>
 #include <terralib/graph/core/AbstractGraph.h>
 #include <terralib/graph/Globals.h>
@@ -34,6 +39,12 @@ TerraLib Team at <terralib-team@terralib.org>.
 
 // Qt
 #include <QMessageBox>
+#include <QFileDialog>
+
+// Boost
+#include <boost/filesystem.hpp>
+#include <boost/uuid/random_generator.hpp>
+#include <boost/uuid/uuid_io.hpp>
 
 
 Q_DECLARE_METATYPE(te::map::AbstractLayerPtr);
@@ -45,7 +56,13 @@ m_ui(new Ui::FlowDiagramDialogForm)
   // add controls
   m_ui->setupUi(this);
 
+  m_ui->m_targetDatasourceToolButton->setIcon(QIcon::fromTheme("datasource"));
+
   //connects
+  connect(m_ui->m_spatialLayerComboBox, SIGNAL(activated(int)), this, SLOT(onSpatialLayerComboBoxActivated(int)));
+  connect(m_ui->m_tabularLayerComboBox, SIGNAL(activated(int)), this, SLOT(onTabularLayerComboBoxActivated(int)));
+  connect(m_ui->m_targetDatasourceToolButton, SIGNAL(pressed()), this, SLOT(onTargetDatasourceToolButtonPressed()));
+  connect(m_ui->m_targetFileToolButton, SIGNAL(pressed()), this, SLOT(onTargetFileToolButtonPressed()));
   connect(m_ui->m_okPushButton, SIGNAL(released()), this, SLOT(onOkPushButtonClicked()));
 }
 
@@ -56,7 +73,32 @@ te::qt::plugins::fiocruz::FlowDiagramDialog::~FlowDiagramDialog()
 
 void te::qt::plugins::fiocruz::FlowDiagramDialog::setLayerList(std::list<te::map::AbstractLayerPtr> list)
 {
+  m_ui->m_spatialLayerComboBox->clear();
+  m_ui->m_tabularLayerComboBox->clear();
 
+  //set layers into combo box
+  std::list<te::map::AbstractLayerPtr>::iterator it = list.begin();
+
+  while (it != list.end())
+  {
+    te::map::AbstractLayerPtr l = *it;
+
+    std::auto_ptr<te::da::DataSetType> dsType = l->getSchema();
+
+    if (dsType->hasGeom())
+      m_ui->m_spatialLayerComboBox->addItem(l->getTitle().c_str(), QVariant::fromValue(l));
+
+    if (!dsType->hasRaster())
+      m_ui->m_tabularLayerComboBox->addItem(l->getTitle().c_str(), QVariant::fromValue(l));
+
+    ++it;
+  }
+
+  if (m_ui->m_spatialLayerComboBox->count() > 0)
+    onSpatialLayerComboBoxActivated(0);
+
+  if (m_ui->m_tabularLayerComboBox->count() > 0)
+    onTabularLayerComboBoxActivated(0);
 }
 
 void te::qt::plugins::fiocruz::FlowDiagramDialog::onOkPushButtonClicked()
@@ -129,4 +171,109 @@ void te::qt::plugins::fiocruz::FlowDiagramDialog::onOkPushButtonClicked()
 
   graph->flush();
 
+}
+
+void te::qt::plugins::fiocruz::FlowDiagramDialog::onSpatialLayerComboBoxActivated(int index)
+{
+  QVariant varLayer = m_ui->m_spatialLayerComboBox->itemData(index, Qt::UserRole);
+
+  te::map::AbstractLayerPtr layer = varLayer.value<te::map::AbstractLayerPtr>();
+
+  m_ui->m_spatialPropertyComboBox->clear();
+
+  //set properties from spatial layer into referency property combo
+  std::auto_ptr<te::da::DataSetType> dsType = layer->getSchema();
+
+  for (std::size_t t = 0; t < dsType->getProperties().size(); ++t)
+  {
+    te::dt::Property* prop = dsType->getProperties()[t];
+
+    if (prop->getType() == te::dt::INT32_TYPE || prop->getType() == te::dt::INT64_TYPE || prop->getType() == te::dt::STRING_TYPE)
+      m_ui->m_spatialPropertyComboBox->addItem(dsType->getProperties()[t]->getName().c_str());
+  }
+}
+
+void te::qt::plugins::fiocruz::FlowDiagramDialog::onTabularLayerComboBoxActivated(int index)
+{
+  QVariant varLayer = m_ui->m_tabularLayerComboBox->itemData(index, Qt::UserRole);
+
+  te::map::AbstractLayerPtr layer = varLayer.value<te::map::AbstractLayerPtr>();
+
+  m_ui->m_tabularOriginComboBox->clear();
+  m_ui->m_tabularDestinyComboBox->clear();
+
+  //set properties from tabular layer into combos
+  std::auto_ptr<te::da::DataSetType> dsType = layer->getSchema();
+
+  for (std::size_t t = 0; t < dsType->getProperties().size(); ++t)
+  {
+    te::dt::Property* prop = dsType->getProperties()[t];
+
+    if (prop->getType() == te::dt::INT32_TYPE || prop->getType() == te::dt::STRING_TYPE)
+    {
+      m_ui->m_tabularOriginComboBox->addItem(dsType->getProperties()[t]->getName().c_str());
+      m_ui->m_tabularDestinyComboBox->addItem(dsType->getProperties()[t]->getName().c_str());
+    }
+  }
+}
+
+void te::qt::plugins::fiocruz::FlowDiagramDialog::onTargetDatasourceToolButtonPressed()
+{
+  m_ui->m_newLayerNameLineEdit->clear();
+  m_ui->m_newLayerNameLineEdit->setEnabled(true);
+
+  te::qt::widgets::DataSourceSelectorDialog dlg(this);
+  dlg.exec();
+
+  std::list<te::da::DataSourceInfoPtr> dsPtrList = dlg.getSelecteds();
+
+  if (dsPtrList.size() <= 0)
+    return;
+
+  std::list<te::da::DataSourceInfoPtr>::iterator it = dsPtrList.begin();
+
+  m_ui->m_repositoryLineEdit->setText(QString(it->get()->getTitle().c_str()));
+
+  m_outputDatasource = *it;
+}
+
+void te::qt::plugins::fiocruz::FlowDiagramDialog::onTargetFileToolButtonPressed()
+{
+  m_ui->m_newLayerNameLineEdit->clear();
+  m_ui->m_repositoryLineEdit->clear();
+
+  QString fileName = QFileDialog::getSaveFileName(this, tr("Save as..."), QString(), tr("Shapefile (*.shp *.SHP);;"), 0, QFileDialog::DontConfirmOverwrite);
+
+  if (fileName.isEmpty())
+    return;
+
+  boost::filesystem::path outfile(fileName.toStdString());
+
+  m_ui->m_repositoryLineEdit->setText(outfile.string().c_str());
+
+  m_ui->m_newLayerNameLineEdit->setText(outfile.leaf().string().c_str());
+
+  m_ui->m_newLayerNameLineEdit->setEnabled(false);
+
+  //create new data source
+  boost::filesystem::path uri(m_ui->m_repositoryLineEdit->text().toStdString());
+
+  std::map<std::string, std::string> dsInfo;
+  dsInfo["URI"] = uri.string();
+
+  boost::uuids::basic_random_generator<boost::mt19937> gen;
+  boost::uuids::uuid u = gen();
+  std::string id_ds = boost::uuids::to_string(u);
+
+  te::da::DataSourceInfoPtr dsInfoPtr(new te::da::DataSourceInfo);
+  dsInfoPtr->setConnInfo(dsInfo);
+  dsInfoPtr->setTitle(uri.stem().string());
+  dsInfoPtr->setAccessDriver("OGR");
+  dsInfoPtr->setType("OGR");
+  dsInfoPtr->setDescription(uri.string());
+  dsInfoPtr->setId(id_ds);
+
+  te::da::DataSourceInfoManager::getInstance().add(dsInfoPtr);
+
+  m_outputDatasource = dsInfoPtr;
 }
